322d991a277bf965c252e9f23d528f5c19750ca4
KairoKorgend <korgendkairo@gmail.com>
2024-06-09 21:31:15 +0300

    changed content

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
+
+document.getElementById(
+  "copyright-year"
+).innerHTML = `Copyright &#169; ${new Date().getFullYear()} Kairo Kõrgend. All rights Reserved.`;
